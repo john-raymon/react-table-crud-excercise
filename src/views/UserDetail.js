4cb0d5ffa0883e9be6f23476f070e263c2a5927f
john-raymon <john@johnraymon.com>
2021-11-28 03:19:28 -0500

    set-up updateUser mutation

--- src/views/UserDetail.js
+++ src/views/UserDetail.js
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import { ROLES_ENUM } from '../enums';
+import { useMutation } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const UPDATE_USER = gql`
+  mutation updateUser($email: ID!, $newAttributes: UserAttributesInput!) {
+    updateUser(email: $email, newAttributes: $newAttributes)  {
+      email
+      role
+    }
+  }
+`;
 
 export default function UserDetail({userEmail, user}) {
   const [email, setEmail] = useState(userEmail);
   const [role, setRole] = useState((user && user.role) || '');
 
+  const [updateUser, { loading, error, data }] = useMutation(UPDATE_USER);
+
   const handleRadioChange = (e) => { 
     setRole(e.target.value);
+  }
+  
+  const onSubmit = (e) => {
+    updateUser({ variables: { email, newAttributes: { name: user.name, role } } });
   }
 
   return (
     <div className="user-detail">
-      <Header title={userEmail} rightElement={<Button>save</Button>}/>
+      <Header title={userEmail} rightElement={<Button onClick={onSubmit}>save</Button>}/>
       <div className="user-detail__inputs-container">
         <div>
           <label id="name" className="text-style__base text-style__base--medium text-style--gray">Name</label>
